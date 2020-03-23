9644e7cd191ca943fdb3f04d93e0d72935d3db55
linux_china <libing.chen@gmail.com>
2020-03-23 15:59:36 -0700

    proto service

---
+++ src/main/java/org/mvnsearch/rsocket/proto/parser/ProtoService.java
@@ -0,0 +1,72 @@
+package org.mvnsearch.rsocket.proto.parser;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * proto service
+ *
+ * @author linux_china
+ */
+public class ProtoService {
+    private String name;
+    private List<ProtoRpc> rpcList = new ArrayList<>();
+
+    public ProtoService(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<ProtoRpc> getRpcList() {
+        return rpcList;
+    }
+
+    public void addRpc(ProtoRpc rpc) {
+        this.rpcList.add(rpc);
+    }
+
+    public String toReactiveServiceDefinition(String packageValue) {
+        String lineSeparator = System.lineSeparator();
+        List<String> importantSentences = new ArrayList<>();
+        importantSentences.add("import reactor.core.publisher.Flux;");
+        importantSentences.add("import reactor.core.publisher.Mono;");
+        Set<String> importedClasses = new HashSet<>();
+        for (ProtoRpc protoRpc : rpcList) {
+            importedClasses.addAll(protoRpc.getImportClasses());
+        }
+        for (String importedClass : importedClasses) {
+            importantSentences.add("import " + importedClass + ";");
+        }
+        List<String> methods = new ArrayList<>();
+        for (ProtoRpc protoRpc : rpcList) {
+            StringBuilder builder = new StringBuilder();
+            if (protoRpc.isServerStreaming()) {
+                builder.append("Flux<" + protoRpc.getReturnType() + ">");
+            } else {
+                builder.append("Mono<" + protoRpc.getReturnType() + ">");
+            }
+            builder.append(" " + protoRpc.getName() + " (");
+            if (protoRpc.isClientStreaming()) {
+                builder.append("Flux<" + protoRpc.getParamType() + ">");
+            } else {
+                builder.append("Mono<" + protoRpc.getParamType() + ">");
+            }
+            builder.append(" obj);");
+            methods.add(builder.toString());
+        }
+        return "package " + packageValue + ";"
+                + lineSeparator + lineSeparator
+                + String.join(lineSeparator, importantSentences)
+                + lineSeparator + lineSeparator
+                + "public interface " + name + " {"
+                + lineSeparator + "  "
+                + String.join(lineSeparator + "  ", methods)
+                + lineSeparator
+                + "}";
+    }
+}
