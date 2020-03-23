8016f24ad9c596907d634b18601df60c1d77baaf
linux_china <libing.chen@gmail.com>
2020-03-23 15:59:24 -0700

    Proto service RPC

---
+++ src/main/java/org/mvnsearch/rsocket/proto/parser/ProtoRpc.java
@@ -0,0 +1,66 @@
+package org.mvnsearch.rsocket.proto.parser;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Proto service RPC
+ *
+ * @author linux_china
+ */
+public class ProtoRpc {
+    private String name;
+    private String paramType;
+    private boolean clientStreaming;
+    private String returnType;
+    private boolean serverStreaming;
+    private Set<String> importClasses = new HashSet<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParamType() {
+        return paramType;
+    }
+
+    public void setParamType(String paramType) {
+        this.paramType = paramType;
+    }
+
+    public boolean isClientStreaming() {
+        return clientStreaming;
+    }
+
+    public void setClientStreaming(boolean clientStreaming) {
+        this.clientStreaming = clientStreaming;
+    }
+
+    public String getReturnType() {
+        return returnType;
+    }
+
+    public void setReturnType(String returnType) {
+        this.returnType = returnType;
+    }
+
+    public boolean isServerStreaming() {
+        return serverStreaming;
+    }
+
+    public void setServerStreaming(boolean serverStreaming) {
+        this.serverStreaming = serverStreaming;
+    }
+
+    public void addImportClass(String importClass) {
+        this.importClasses.add(importClass);
+    }
+
+    public Set<String> getImportClasses() {
+        return importClasses;
+    }
+}
